a105c4dcd2496fd4d60a11528314e0326b6c41be
nikhil <nikhilsahani242003@gmail.com>
2024-10-06 18:31:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/nikhil', (req, res) => {
+    res.send('<h5>use me</h5>')
+  })
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
+})
